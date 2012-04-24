db351346c358b025de8da9156e3e73f713efdd50
George Malamidis <george@nutrun.com>
2012-04-24 07:27:48 -0500

    http interdace stuff

--- http.go
+++ http.go
@@ -25,28 +25,27 @@ func (this *HttpDatastore) url(path string) string {
 	return fmt.Sprintf("http://%s%s", this.host, path)
 }
 
-func (this *HttpDatastore) Put(local, remote string) error {
+func (this *HttpDatastore) Put(local, remote string) (io.ReadCloser, error) {
 	f, e := os.Open(local)
 	if e != nil {
-		return e
+		return nil, e
 	}
 	defer f.Close()
 	r := bufio.NewReader(f)
 	req, e := http.NewRequest("PUT", this.url(remote), r)
 	info, e := f.Stat()
 	if e != nil {
-		return e
+		return nil, e
 	}
 	req.ContentLength = info.Size()
 	if e != nil {
-		return e
+		return nil, e
 	}
 	resp, e := http.DefaultTransport.RoundTrip(req)
 	if e != nil {
-		return e
+		return nil, e
 	}
-	this.writeBody(resp.Body)
-	return nil
+	return resp.Body, nil
 }
 
 func (this *HttpDatastore) Get(remote string) (io.ReadCloser, error) {
@@ -57,30 +56,18 @@ func (this *HttpDatastore) Get(remote string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
-func (this *HttpDatastore) Delete(remote string) error {
+func (this *HttpDatastore) Delete(remote string) (io.ReadCloser, error) {
 	req, err := http.NewRequest("DELETE", this.url(remote), nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	resp, err := http.DefaultTransport.RoundTrip(req)
-	this.writeBody(resp.Body)
-	return err
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
 }
 
 func (this *HttpDatastore) Ls() []string {
 	return make([]string, 0)
-}
-
-func (this *HttpDatastore) writeBody(in io.ReadCloser) {
-	defer in.Close()
-	r := bufio.NewReader(in)
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
-	for {
-		line, e := r.ReadSlice('\n')
-		if e != nil {
-			break
-		}
-		out.Write(line)
-	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"io"
 	"log"
 	"os"
 )
@@ -19,31 +21,47 @@ func main() {
 			flag.Usage()
 			os.Exit(1)
 		}
-		err := http.Put(args[0], args[1])
+		body, err := http.Put(args[0], args[1])
 		if err != nil {
 			log.Fatal(err)
+			WriteBody(body, os.Stderr)
 		}
 	} else if *del {
 		if len(args) != 1 {
 			flag.Usage()
 			os.Exit(1)
 		}
-		err := http.Delete(args[0])
+		body, err := http.Delete(args[0])
 		if err != nil {
 			log.Fatal(err)
+			WriteBody(body, os.Stderr)
 		}
 	} else if *get {
 		if len(args) != 1 {
 			flag.Usage()
 			os.Exit(1)
 		}
-		in, err := http.Get(args[0])
+		body, err := http.Get(args[0])
 		if err != nil {
 			log.Fatal(err)
 		}
-		http.writeBody(in)
+		WriteBody(body, os.Stdout)
 	} else {
 		flag.Usage()
 		os.Exit(1)
+	}
+}
+
+func WriteBody(in io.ReadCloser, out io.Writer) {
+	defer in.Close()
+	r := bufio.NewReader(in)
+	bout := bufio.NewWriter(out)
+	defer bout.Flush()
+	for {
+		line, e := r.ReadSlice('\n')
+		if e != nil {
+			break
+		}
+		bout.Write(line)
 	}
 }
