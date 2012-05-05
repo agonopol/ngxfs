fd22771fdbeff9c74f61e1ad935be92a283a1736
George Malamidis <george@nutrun.com>
2012-05-04 21:14:48 -0500

    func test runs from everywhere

--- test/test.sh
+++ test/test.sh
@@ -9,22 +9,23 @@ function fail {
 }
 
 server=http://0.0.0.0:8080
+here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "testing on $server"
 curl -I --silent $server/A/hi.txt | grep -q 404 || fail "Shouldn't have found /A/hi.txt"
 curl -I --silent $server/B/hi.txt | grep -q 404 || fail "Shouldn't have found /B/hi.txt"
 curl -I --silent $server/C/hi.txt | grep -q 404 || fail "Shouldn't have found /C/hi.txt"
 echo "testing put"
-NGXFS_CONF=./ngxfs.conf ngxfs -put hi.txt /hi.txt
+NGXFS_CONF=$here/ngxfs.conf ngxfs -put $here/hi.txt /hi.txt
 curl -I --silent $server/A/hi.txt | grep -q 404 || fail "Shouldn't have found /A/hi.txt"
 curl -I --silent $server/B/hi.txt | grep -q 200 || fail "Should have found /B/hi.txt"
 curl -I --silent $server/C/hi.txt | grep -q 200 || fail "Should have found /C/hi.txt"
 echo "testing get"
-NGXFS_CONF=./ngxfs.conf ngxfs /hi.txt | grep -q helo || fail "Didn't find /hi.txt"
+NGXFS_CONF=$here/ngxfs.conf ngxfs /hi.txt | grep -q helo || fail "Didn't find /hi.txt"
 echo "testing ls"
-NGXFS_CONF=./ngxfs.conf ngxfs -ls / | grep -q "hi.txt" || fail "Incorrect ls list"
+NGXFS_CONF=$here/ngxfs.conf ngxfs -ls / | grep -q "hi.txt" || fail "Incorrect ls list"
 echo "testing del"
-NGXFS_CONF=./ngxfs.conf ngxfs -del /hi.txt
+NGXFS_CONF=$here/ngxfs.conf ngxfs -del /hi.txt
 curl -I --silent $server/A/hi.txt | grep -q 404 || fail "Shouldn't have found /A/hi.txt"
 curl -I --silent $server/B/hi.txt | grep -q 404 || fail "Shouldn't have found /B/hi.txt"
 curl -I --silent $server/C/hi.txt | grep -q 404 || fail "Shouldn't have found /C/hi.txt"
