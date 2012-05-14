85681671f2a7b9000eeda2af70cf730e63447df4
George Malamidis <george@nutrun.com>
2012-05-14 07:56:27 -0500

    fixed install script to run from everywhere

--- etc/install-nginx.sh
+++ etc/install-nginx.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+here=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $here
+
 wget http://nginx.org/download/nginx-1.2.0.tar.gz
 tar -xvf nginx-1.2.0.tar.gz
 rm nginx-1.2.0.tar.gz
@@ -11,10 +14,9 @@ cd nginx-1.2.0
 ./configure --with-http_dav_module --without-http_rewrite_module
 make
 make install
-cd ../
 mkdir -p /tmp/nginx
 mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.orig
-cp ./nginx.conf /usr/local/nginx/conf/
+cp $here/nginx.conf /usr/local/nginx/conf/
 cd /usr/local/nginx/
 sbin/nginx
 curl -I http://0.0.0.0:8080/
